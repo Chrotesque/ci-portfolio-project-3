7689b23960ef28d66fa96511f70705bdfd34ee7a
Chrotesque <github@chrotesque.com>
2022-03-31 03:30:31 +0000

    Add base_map, visible_map modules & base structure

---
+++ base_map.py
@@ -0,0 +1,53 @@
+class BaseMap:
+    def __init__():
+        ""
+
+    def set_base_map():
+        """
+        Creates the base map layout
+        """
+
+    def set_entry():
+        """
+        Decides and sets the entry into the level based on level
+        """
+
+    def set_path():
+        """
+        Creates and sets the main path through the level
+        """
+
+    def set_exit():
+        """
+        Set down the level exit at the end of the main path
+        """
+
+    def set_branches():
+        """
+        Creates and sets branches going off of the main path
+        """
+
+    def get_accessible_rooms():
+        """
+        Collects all accessible rooms for entity distribution
+        """
+
+    def set_items():
+        """
+        Set down all collectible items within accessible rooms
+        """
+
+    def set_enemies():
+        """
+        Set down enemy encounters within accessible rooms
+        """
+
+    def set_npcs():
+        """
+        Set down NPCs like vendors within accessible rooms        
+        """
+
+    def set_event():
+        """
+        Set down events within accessible rooms
+        """
---
+++ visible_map.py
@@ -0,0 +1,28 @@
+class VisibleMap:
+    def __init__():
+        ""
+
+    def set_visible_map():
+        """
+        Creates an empty map
+        """
+
+    def reveal_area():
+        """
+        Reveals the portion the player is in based off of the base map
+        """
+
+    def get_avail_movement():
+        """
+        Get all available movement options based off of base map
+        """
+
+    def get_avail_actions():
+        """
+        Get all available actions based off of base map
+        """
+
+    def set_new_pos():
+        """
+        
+        """
